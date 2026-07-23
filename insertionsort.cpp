2c938e7a63c98f2463d20a720e9c71b8028d050e
HRUSHIKESH <muskuhrushikesh.128762@marwadiuniversity.ac.in>
2026-07-23 18:09:21 +0530

    Create insertionsort.cpp

---
+++ insertionsort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {34, 56, 23, 13, 78, 96};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
